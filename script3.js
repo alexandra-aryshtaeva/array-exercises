77d2fdeec53b3baec0d159c3a4e9f822a88d9bf9
Alexandra Aryshtaeva <alexandra.maranga@gmail.com>
2024-01-18 15:23:33 +0000

    do i finally knwo what a palindrome is

--- script3.js
+++ script3.js
@@ -1,34 +1,57 @@
 const randomWords = [
   "apple",
   "banana",
-  "level",
+  "Level",
   "orange",
   "madam",
   "kiwi",
-  "civic",
+  "Civic",
   "grape",
   "radar",
-  "strawberry",
+  "Strawberry",
   "noon",
-  "watermelon",
+  "Watermelon",
   "deed",
   "mango",
   "rotor",
 ];
+// let i = newWord.length - 1; i > 0; i--
 
 // 1.Filter Palindromes:
 // Write a function that uses the filter method to return an array of words from an array of strings, keeping only those that are palindromes.
 
-// const wordTest = isPalindrome();
+function isPalindrome(word) {
+  let lowerCaseWord = word.toLowerCase();
 
-// function isPalindrome() {
-//   return randomWords.filter((e) => {
-//     if
-//   });
-// }
+  //new word its the reversed word which technaclly doesnt exists yet
+  let newWord = "";
+  // so we take the "old word" LowerCaseWord and we put it on loop
+  // the loop satrts at the end of the word and finishes at the beggining
+  // each cycle of the loop returns a letter[i] (index[i])
+  //  and where the letters[i] will go?
+  for (let i = lowerCaseWord.length - 1; i >= 0; i--) {
+    // the letters are going to this new variable
+    //ex: (mango) something like this is happening:
+    //newWord += o
+    //newWord += og
+    //newWord += ogn
+    //newWord += ogna
+    //newWord += ognam
 
-// console.log(wordTest);
+    newWord += lowerCaseWord[i];
+  }
+  // outside the loop this how we verify if its a palindrome,
+  // the lowerCaseWord is the input that the user puts
+  // the newWord is the reversed word
+  //to be apalindrom the reversed word and the inputed word have to be the same
+  return newWord === lowerCaseWord;
+}
 
+const wordTest = randomWords.filter(isPalindrome);
+
+//console.log(wordTest);
+
+//
 const people = [
   { name: "John", age: 25, city: "New York" },
 
@@ -38,11 +61,32 @@ const people = [
   { name: "Charlie", age: 35, city: "Seattle" },
 ];
 
-const names = pickName();
+// If i just want the property i cant use filter to return the property it will only return objects
 
-function pickName() {
+// const names = pickName("name");
+
+// function pickName(property) {
+//   return people.map((person) => {
+
+//     if (property === "name") {
+//       return person.name;
+//     } else if (property === "age") {
+//       return person.age;
+//     } else if (property === "city") {
+//       return person.city;
+//     } else return false;
+//   });
+// }
+
+const names = pickName("city");
+
+function pickName(property) {
   return people.map((person) => {
-    return person.name;
+    const newPerson = {};
+
+    newPerson[property] = person[property];
+
+    return newPerson;
   });
 }
 
